b900d4a6d864216177111ab23ff5efc539e9e87e
danm444 <141696819+danm444@users.noreply.github.com>
2026-01-16 22:07:52 -0800

    Add files via upload

---
+++ VarsAndMath.java
@@ -0,0 +1,13 @@
+public class VarsAndMath {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.5;
+        double sum = a + b;
+        double product = a * b;
+
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        System.out.println("The product of " + a + " and " + b + " is " + product);
+
+
+    }
+}
